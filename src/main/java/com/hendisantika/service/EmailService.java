963a0d3cbfcc3026180703f23019791b9b14d2a7
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-12 20:10:12 +0700

    #8 - Add sendEmail in EmailService

--- src/main/java/com/hendisantika/service/EmailService.java
+++ src/main/java/com/hendisantika/service/EmailService.java
@@ -1,8 +1,17 @@
 package com.hendisantika.service;
 
+import com.hendisantika.dto.EmailRequest;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,5 +27,34 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     @Autowired
     private SendGrid sendGrid;
+
+    public Response sendEmail(EmailRequest emailrequest) {
+
+        Mail mail = new Mail(new Email("hendisantika@yahoo.co.id"), emailrequest.getSubject(), new Email(emailrequest.getTo()), new Content("text/plain", emailrequest.getBody()));
+        mail.setReplyTo(new Email("hendisantika@yahoo.co.id"));
+        Request request = new Request();
+
+        Response response = null;
+
+        try {
+
+            request.setMethod(Method.POST);
+
+            request.setEndpoint("mail/send");
+
+            request.setBody(mail.build());
+
+            response = this.sendGrid.api(request);
+
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+
+        }
+
+        return response;
+
+    }
+
 
 }
