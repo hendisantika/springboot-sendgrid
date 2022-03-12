43d977fba44fff5514549a854d9e8955099c36ca
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-12 20:12:35 +0700

    #10 - Add sendEmail in EmailController

--- src/main/java/com/hendisantika/controller/EmailController.java
+++ src/main/java/com/hendisantika/controller/EmailController.java
@@ -1,8 +1,14 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.EmailRequest;
 import com.hendisantika.service.EmailService;
+import com.sendgrid.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +25,14 @@ public class EmailController {
 
     @Autowired
     private EmailService emailService;
+
+    @PostMapping("/sendemail")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailrequest) {
+
+        Response response = emailService.sendEmail(emailrequest);
+        if (response.getStatusCode() == 200 || response.getStatusCode() == 202)
+            return new ResponseEntity<>("send successfully", HttpStatus.OK);
+        return new ResponseEntity<>("failed to sent", HttpStatus.NOT_FOUND);
+
+    }
 }
